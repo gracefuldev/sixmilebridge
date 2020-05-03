4de8096c30f3ba8efff78cc6a42b1d355587a001
Jessica Kerr <jessitron@gmail.com>
2020-05-03 12:53:03 -0500

    Add index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root "home#index"
+
   resources :islanders
 
-  get '/invite', to: 'invitations#new'
-  
+  get "/invite", to: "invitations#new"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
