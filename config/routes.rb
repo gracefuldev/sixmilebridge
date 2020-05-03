9fd42f4c39d0f9e0fa0baa39f1807c65bf807294
Jessica Kerr <jessitron@gmail.com>
2020-05-03 13:40:26 -0500

    Prepare for public release

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   get "/herewego", to: "acceptances#new"
 
   # Logged-in islander (user) interfaces
+  # this doesn't do anything yet so it's public too right now
   get "/invite", to: "invitations#new"
 
-  # Operator interfaces
-  resources :islanders
+  # Operator interfaces (like for us)
+  scope :operator do
+    # removing until we can secure it
+    # resources :islanders
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
