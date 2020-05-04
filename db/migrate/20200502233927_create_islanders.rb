c777ea177e4663e0eb919cb64076a8c533bb1cba
Avdi Grimm <avdi@avdi.codes>
2020-05-04 00:44:29 +0000

    Undo rename in migration

--- db/migrate/20200502233927_create_islanders.rb
+++ db/migrate/20200502233927_create_islanders.rb
@@ -2,7 +2,7 @@ class CreateIslanders < ActiveRecord::Migration[6.0]
   def change
     create_table :islanders do |t|
       t.string :email
-      t.string :timezone
+      t.string :timezzone
 
       t.timestamps
     end
