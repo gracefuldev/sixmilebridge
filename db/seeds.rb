ba1eff02e3b17fbd5a275350da4a1703358dd902
Jessica Kerr <jessitron@gmail.com>
2020-06-02 20:13:24 +0000

    Add me so I can log in with Google

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Islander.find_or_create_by(email: "avdi@avdi.codes") do |avdi|
-  avdi.timezone = "(GMT-06:00) Central Time (US & Canada)"
+  avdi.timezone = "(GMT-05:00) Eastern Time (US & Canada)"
+end
+
+Islander.find_or_create_by(email: "jessitron@jessitron.com") do |jess|
+  jess.timezone = "(GMT-06:00) Central Time (US & Canada)"
 end
