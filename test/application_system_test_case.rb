ab72961348192e8cdb8dd7b80390b907eb062685
Avdi Grimm <avdi@avdi.codes>
2020-06-02 23:38:17 +0000

    Move Capybara config to application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
+Capybara.server_host = ENV.fetch("CAPYBARA_SERVER_HOST")
+Capybara.server_port = ENV.fetch("CAPYBARA_SERVER_PORT")
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   parallelize(workers: 1)
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,6 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-Capybara.server_host = ENV.fetch("CAPYBARA_SERVER_HOST")
-Capybara.server_port = ENV.fetch("CAPYBARA_SERVER_PORT")
-
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
