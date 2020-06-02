bb65097780ac9bf7520cac0bdc16d03778fc1337
Jessica Kerr <jessitron@gmail.com>
2020-06-02 21:46:42 +0000

    Extract login tracing into separate methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
 
   def create
-    Honeycomb.start_span(name: "login") do |span|
-      Honeycomb.add_field("auth.provider", auth_provider)
+    trace_login_attempt do
       @islander = Islander.find_by(email: authorized_email)
       if (@islander)
-        Honeycomb.add_field_to_trace("islander_id", @islander.id)
-        Honeycomb.add_field("result", "success")
+        trace_login_success
         self.acting_islander = @islander
         redirect_to desired_path
       else
-        Honeycomb.add_field("rejected_email", authorized_email)
-        Honeycomb.add_field("result", "failure")
-        Honeycomb.add_field("notice", "rejected new islander")
+        trace_login_failure
         redirect_to "/no-new-accounts"
       end
     end
@@ -36,5 +32,23 @@ class SessionsController < ApplicationController
 
   def desired_path
     request.env["omniauth.origin"] or "/"
+  end
+
+  def trace_login_attempt
+    Honeycomb.start_span(name: "login") do |span|
+      Honeycomb.add_field("auth.provider", auth_provider)
+      yield
+    end
+  end
+
+  def trace_login_success
+    Honeycomb.add_field_to_trace("islander_id", @islander.id)
+    Honeycomb.add_field("result", "success")
+  end
+
+  def trace_login_failure
+    Honeycomb.add_field("rejected_email", authorized_email)
+    Honeycomb.add_field("result", "failure")
+    Honeycomb.add_field("notice", "rejected new islander")
   end
 end
