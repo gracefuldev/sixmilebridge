eb91e72f5600dfa54438b4de6139348547bd9904
Jessica Kerr <jessitron@gmail.com>
2020-06-02 16:19:30 -0500

    middleware: 'ensure' that the request always keeps going

--- config/initializers/honeycomb_custom.rb
+++ config/initializers/honeycomb_custom.rb
@@ -1,12 +1,13 @@
-class FilterLocalHost
+class HoneycombCustom
   def initialize(app)
     @app = app
   end
 
   def call(env)
     Honeycomb.add_field("request.verb", env["REQUEST_METHOD"])
+  ensure
     @app.call(env)
   end
 end
 
-Rails.application.config.middleware.use FilterLocalHost
+Rails.application.config.middleware.use HoneycombCustom
