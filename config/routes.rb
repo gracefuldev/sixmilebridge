5ac4b15000f74af5203800a8e18ee010514522c9
Jessica Kerr <jessitron@gmail.com>
2020-06-02 20:13:14 +0000

    Google does a GET to this callback
    while the omniauth developer thinger does a post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # authinate
   # it said to make this a get ... but for developer it needs to be a post. what is it for others?
-  post "/auth/:provider/callback", to: "sessions#create"
+  match "/auth/:provider/callback", to: "sessions#create", via: [:post, :get]
 
   get "/logout", to: "sessions#delete"
 
