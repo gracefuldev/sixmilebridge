b5e4228773723be220a68d624be6cddc5b5441ed
Jessica Kerr <jessitron@gmail.com>
2020-06-01 20:56:23 -0500

    Well we found the email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create unless Rails.env.production?
 
   def create
-    @islander = Islander.find_or_create_from_auth_hash(auth_hash)
+    @islander = Islander.find_by(email: authorized_email) # well, it works for "developer"
     self.current_islander = @islander
     redirect_to "/"
   end
 
   protected
 
-  def auth_hash
-    request.env["omniauth.auth"]
+  def authorized_email
+    request.env.dig("omniauth.auth", :info, :email)
   end
 end
