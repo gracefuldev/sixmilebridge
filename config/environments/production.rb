ed31f824c8ff62e00fb04e12bba7a09e38ec1d58
Jessica Kerr <jessitron@gmail.com>
2020-07-28 02:06:52 +0000

    Update gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  # some comment when I did a 'bundle update' said maybe this should be:
+  # config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
