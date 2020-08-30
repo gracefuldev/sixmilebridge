886fb290a0bed3c00dcc3b012e14c39f446da6b9
Jessica Kerr <jessitron@gmail.com>
2020-08-30 10:12:11 -0500

    Comments to tell the story

--- config/initializers/honeycomb_custom.rb
+++ config/initializers/honeycomb_custom.rb
@@ -4,6 +4,9 @@ class HoneycombCustom
   end
 
   def call(env)
+    # This isn't needed; I found the field in the standard place. 
+    # (I forgot what it was called. 'method' maybe)
+    # but, we'll leave this here as a place to add custom fields.
     Honeycomb.add_field("request.verb", env["REQUEST_METHOD"])
     @app.call(env)
   end
