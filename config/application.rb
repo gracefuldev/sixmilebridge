1d9d48c7a7784c6d4ba7e2e7f531847744dbc00f
Jessica Kerr <jessitron@gmail.com>
2020-10-04 22:09:32 +0000

    Revert "Why do .erb changes not show up"
    It doesn't fix the problem
    
    This reverts commit 1dec3e756c047a7238c154ac6dd940f71568a547.

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ module Sixmilebridge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.autoload = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
