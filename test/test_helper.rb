19d85266d7675635ac1c878140b80eb0aab7dfd7
Jessica Kerr <jessitron@gmail.com>
2020-10-04 00:25:28 +0000

    Make the system tests work again
    They started running in parallel, and I don't want them to.
    They get "port already in use" yeah no shit, stop stepping on yourself
    
    We don't have enough tests to want them to run in parallel yet anyway.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: 1) # no. just don't. The system tests keep failing with "port already in use"
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
