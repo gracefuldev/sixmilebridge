565cae6d657a93236c36f287a6ff18248fdf330e
Jessica Kerr <jessitron@gmail.com>
2020-11-01 14:17:15 -0600

    A legit admin page
    There's nothing on it, but it renders

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,6 @@ class AdminController < ApplicationController
   def index
     return render_not_found if acting_islander.nil?
     return render_not_found unless admin?(acting_islander)
-    render plain: "Yay, you found me"
   end
 
   def admin?(islander)
